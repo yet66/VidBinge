285e66fd58e6a6a8031513fbd4d12f6d76963b99
yet66 <85257710+yet66@users.noreply.github.com>
2024-09-30 17:38:53 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.waweloc333.workers.dev/",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTk2MWU4Y2I1Nzc4MzEwODM2YzQwNDFiYjI3MmEyNCIsIm5iZiI6MTcyNzczNTkyMC42MTQ4ODIsInN1YiI6IjY2ZmEzZGEyMGExMjIxNTYzMWNjN2U1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M90GZNInHSrlCADeYBicTiV14nTPcQhHPLhpaRhkZGo",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
